bdf066677bad4cb113b9339e78423a75e647857a
Arlysthon Feitosa <66177385+ArlysthonFeitosa@users.noreply.github.com>
2021-09-10 14:07:11 -0300

    Dao and Repository

--- app/src/main/java/com/arlysfeitosa/emprestimosapp/services/repository/LoansDAO.kt
+++ app/src/main/java/com/arlysfeitosa/emprestimosapp/services/repository/LoansDAO.kt
@@ -1,7 +1,33 @@
 package com.arlysfeitosa.emprestimosapp.services.repository
 
-import androidx.room.Dao
+import androidx.room.*
+import com.arlysfeitosa.emprestimosapp.services.model.LoanModel
 
 @Dao
 interface LoansDAO {
+
+    @Insert
+    fun saveLoan(loan: LoanModel): Long
+
+    @Update
+    fun updateLoan(loan: LoanModel): Int
+
+    @Delete
+    fun deleteLoan(loan: LoanModel)
+
+    @Query("SELECT * FROM loans")
+    fun getAllLoans(): List<LoanModel>
+
+    @Query("SELECT * FROM loans WHERE id = :id")
+    fun getLoanById(id: Int): LoanModel
+
+    @Query("SELECT id From loans WHERE date IN (:dateList)")
+    fun selectIdWhereDateIn(dateList: List<String>): List<Int>
+
+    @Query("SELECT * From loans WHERE id IN (:idList)")
+    fun selectWhereIdIn(idList: List<Int>): List<LoanModel>
+
+    @Query("DELETE FROM loans WHERE date NOT IN (:dateList)")
+    fun deleteWhereNotIn(dateList: List<String>)
+
 }
---
+++ app/src/main/java/com/arlysfeitosa/emprestimosapp/services/repository/Repository.kt
@@ -0,0 +1,42 @@
+package com.arlysfeitosa.emprestimosapp.services.repository
+
+import android.content.Context
+import com.arlysfeitosa.emprestimosapp.services.model.LoanModel
+
+class Repository(context: Context) {
+
+    private val mLoansDataBase = LoansDataBase.getDataBase(context).loansDao()
+
+    fun saveLoan(loan: LoanModel): Boolean {
+        return mLoansDataBase.saveLoan(loan) > 0
+    }
+
+    fun updateloan(loan:LoanModel){
+        mLoansDataBase.updateLoan(loan)
+    }
+
+    fun deleteLoan(loan: LoanModel){
+        mLoansDataBase.deleteLoan(loan)
+    }
+
+    fun getAllLoans():List<LoanModel>{
+        return mLoansDataBase.getAllLoans()
+    }
+
+    fun getLoanById(id:Int): LoanModel{
+        return mLoansDataBase.getLoanById(id)
+    }
+
+    fun getLoansInIdList(list: List<Int>): List<LoanModel>{
+        return mLoansDataBase.selectWhereIdIn(list)
+    }
+
+    fun getLoansIdInDateList(list: List<String>): List<Int>{
+        return mLoansDataBase.selectIdWhereDateIn(list)
+    }
+
+    fun deleteWhereNotInDates(dateList: List<String>){
+        mLoansDataBase.deleteWhereNotIn(dateList)
+    }
+
+}
